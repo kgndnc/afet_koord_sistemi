9f5f5264807a202a5387684598716ab5cdf530dd
kgndnc <hkagandnc@gmail.com>
2023-06-02 12:24:18 +0300

    last changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,54 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useStateValue } from './StateProvider'
 
 import './App.css'
 import AdminTag from './components/AdminTag'
 
 function App() {
-  const [{ isAdmin }, dispatch] = useStateValue()
-  const navigate = useNavigate()
-
-  console.log(import.meta.env.BASE_URL)
+  const classNameForBtns =
+    'inline-block w-60 rounded py-20 hover:opacity-80 hover:text-3xl duration-300 transition-all'
 
   return (
     <div className="App">
       <AdminTag />
-      <nav className="mt-4 text-center">
-        <ul className="space-y-2">
-          <li>
-            <Link to={'/'}>Anasayfa</Link>
-          </li>
-          <li>
-            <Link to={'/adres-ihbar'}>Adres İhbar</Link>
-          </li>
-          <li>
-            <Link to={'/login'}>Yönetici Girişi</Link>
-          </li>
 
-          <li>
-            {/* <Link to={import.meta.env.BASE_URL + 'bilgilendirme2.html'}>
+      <h1 className="mb-12 mt-12 text-center font-serif text-3xl font-bold ">
+        Afet Bilgilendirme ve Yardım Koordinasyon Sistemi
+      </h1>
+      <nav className="mx-auto mt-4 text-center font-sans text-lg">
+        <div className="mx-auto flex w-3/4 max-w-xl flex-wrap justify-center gap-3 ">
+          <Link className={`${classNameForBtns} bg-sky-400`} to={'/'}>
+            Anasayfa
+          </Link>
+
+          <Link
+            className={`${classNameForBtns} bg-red-400`}
+            to={'/adres-ihbar'}
+          >
+            Adres İhbar
+          </Link>
+
+          <Link className={`${classNameForBtns} bg-slate-500 `} to={'/login'}>
+            Yönetici Girişi
+          </Link>
+
+          {/* <Link to={import.meta.env.BASE_URL + 'bilgilendirme2.html'}>
               Bilgilendirme
             </Link> */}
 
-            <a href="/bilgilendirme2.html">Bilgilendirme</a>
-          </li>
-          <li>
-            <a href="/gerekli_malzeme.html">Gerekli Yardım Malzemeleri</a>
-          </li>
-        </ul>
+          <a
+            className={`${classNameForBtns} bg-lime-700`}
+            href="/bilgilendirme2.html"
+          >
+            Bilgilendirme
+          </a>
+
+          <a
+            className={`${classNameForBtns} self-end bg-indigo-400`}
+            href="/gerekli_malzeme.html"
+          >
+            Gerekli Yardım Malzemeleri
+          </a>
+        </div>
       </nav>
     </div>
   )
